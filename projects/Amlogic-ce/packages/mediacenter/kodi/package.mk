5eb6174f5dd38acb0ef51b5148525690c337a9a8
SamuriHL <samurihl@msn.com>
2026-08-03 12:13:03 -0400

    kodi: point package at coreelec-xbmc 5be3971b0

--- projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="e6b11086ea88e40b505f1f244a7d7e478877ca82"
+PKG_VERSION="5be3971b0a14c3c03de7e31907b8c232107aa5c1"
 PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
