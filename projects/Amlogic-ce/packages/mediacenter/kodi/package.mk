553d75d9937465389ffa0a1254a95f25d80e1366
SamuriHL <samurihl@msn.com>
2026-08-04 16:24:49 -0400

    kodi: re-pin to fbea84417c (adds the ScreenshotSurfaceAML build fix)

--- projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="3d523499149abe4097bda4ea4d0e739d94f3ab7f"
+PKG_VERSION="fbea84417c62228bc051fce568f301b634b5ba95"
 PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
