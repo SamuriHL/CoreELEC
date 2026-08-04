0b2ef3f2dd981660ee4adcbd3090022bc8fbd46d
SamuriHL <samurihl@msn.com>
2026-08-03 22:36:51 -0400

    kodi: point package at coreelec-xbmc 33a1b8312c
    Picks up the RenderManager m_lateframes latch fix. Queued - not released;
    the current public build remains v22.0-samurihl-20260803202836.

--- projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="64f9d25052aedd9afd72c5cd2b342220a5c2d846"
+PKG_VERSION="33a1b8312c3d6fd25cfb0212bc9dd2ad699e383b"
 PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
