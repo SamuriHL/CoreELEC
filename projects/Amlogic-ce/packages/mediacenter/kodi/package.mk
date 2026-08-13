f5646ef37208df716882595e3e2b5f98d301bf68
SamuriHL <samurihl@msn.com>
2026-08-13 11:11:14 -0400

    kodi: point package at coreelec-xbmc fork, not stock
    Re-establishes the fork pointer and pins to the post-rebase xbmc tip
    e94c6dc9c6. The two prior pin commits were pin-only and superseded by
    this one.

--- projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
+++ projects/Amlogic-ce/packages/mediacenter/kodi/package.mk
@@ -4,11 +4,11 @@
 # Copyright (C) 2022-present Team CoreELEC (https://coreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="3de1f35efd63b69ad2e6cd0bb8d6f637c9d92f4d"
+PKG_VERSION="b8f7a1159c768184791415cdd1ca9f771744f6d8"
 PKG_SHA256=""
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
-PKG_URL="https://github.com/CoreELEC/xbmc/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/SamuriHL/coreelec-xbmc/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain JsonSchemaBuilder:host TexturePacker:host Python3 zlib systemd lzo pcre2 swig:host libass curl exiv2 fontconfig fribidi tinyxml tinyxml2 libjpeg-turbo freetype libcdio taglib libxml2 libxslt nlohmann-json sqlite ffmpeg crossguid libfmt lirc libfstrcmp flatbuffers:host flatbuffers libudfread spdlog obu_util libdovi"
 PKG_DEPENDS_UNPACK="commons-lang3 commons-text groovy"
 PKG_DEPENDS_HOST="toolchain"
